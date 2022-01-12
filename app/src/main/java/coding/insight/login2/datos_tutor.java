8dfceb16c80b5ec92ecaa89ffd4f04a53282e8c2
Jeffrey <jeffreycaco_98@hotmail.com>
2022-01-12 02:06:50 -0500

    Subiendo cambio Jeffcaco - v2

---
+++ app/src/main/java/coding/insight/login2/datos_tutor.java
@@ -0,0 +1,14 @@
+package coding.insight.login2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class datos_tutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.datos_tutor);
+    }
+}
